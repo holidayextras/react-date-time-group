edfc534075f2644f79fa9d31f7885ecb8d8fa375
Ollie Buck <git@olliebuck.com>
2015-10-09 11:45:28 +0100

    progress - locales not working as intended

--- doc/example.js
+++ doc/example.js
@@ -1,16 +1,65 @@
 'use strict';
+/* eslint-disable no-alert */
 
 var React = require('react');
 var DateTimeGroup = require('../src/DateTimeGroup.jsx');
+var myDate = new Date('2015-05-05T09:00');
 
-React.render(
-  <div>
-    <h1>Defaults</h1>
-    <DateTimeGroup />
+var myRestrictedDate = new Date('2015-05-10T09:00');
+var minDate = new Date('2015-05-03T00:00');
+var maxDate = new Date('2015-05-16T00:00');
+var excludedDates = [
+  new Date('2015-05-05T00:00'),
+  new Date('2015-05-06T00:00'),
+  new Date('2015-05-12T00:00'),
+  new Date('2015-05-15T00:00')
+];
 
-    <h1>Date Only</h1>
-    <DateTimeGroup includeTime={false} />
-  </div>
-  ,
-  document.getElementById('container')
-);
+var changeDate = function(newDate) {
+  myDate = newDate;
+  render();
+};
+
+var render = function() {
+  React.render(
+    <div>
+      <h1>Defaults</h1>
+      <DateTimeGroup />
+
+      <h1>Date Only</h1>
+      <DateTimeGroup includeTime={false} />
+
+      <h1>Options for time (value shared with "Events")</h1>
+      <DateTimeGroup 
+        value={myDate}
+        onChange={changeDate}
+        timeClassName="myClass"
+        timeLabel="Choose a time"
+        timeName="TheTime"
+        timeStart={330}
+        timeEnd={2130}
+        timeStep={15} />
+
+      <h1>Options for date</h1>
+      <DateTimeGroup
+        value={myRestrictedDate}
+        dateClassName="myClass"
+        dateLabel="Choose a date"
+        dateName="TheDate"
+        dateStart={minDate}
+        dateEnd={maxDate}
+        dateExclusions={excludedDates}
+        dateFormat="MMMM Do YYYY" />
+
+      <h1>Events (value shared with "Options for time")</h1>
+      <DateTimeGroup value={myDate} onChange={changeDate} />
+
+      <h1>Localization</h1>
+      <DateTimeGroup value={myDate} onChange={changeDate} locales={['en-US']} />
+    </div>
+    ,
+    document.getElementById('container')
+  );
+};
+
+render();
--- src/DateTimeGroup.jsx
+++ src/DateTimeGroup.jsx
@@ -9,25 +9,90 @@ var React = require('react');
 var TimePicker = require('react-time-select');
 var DatePicker = require('react-datepicker');
 var ReactBootstrap = require('react-bootstrap');
+var moment = require('moment');
 
 var DateTimeGroup = React.createClass({
   propTypes: {
-    includeTime: React.PropTypes.bool
+    includeTime: React.PropTypes.bool,
+    timeClassName: React.PropTypes.string,
+    timeLabel: React.PropTypes.string,
+    timeName: React.PropTypes.string,
+    value: React.PropTypes.instanceOf(Date),
+    onChange: React.PropTypes.func,
+    timeStart: React.PropTypes.number,
+    timeEnd: React.PropTypes.number,
+    timeStep: React.PropTypes.number,
+    dateName: React.PropTypes.string,
+    dateLabel: React.PropTypes.string,
+    dateStart: React.PropTypes.instanceOf(Date),
+    dateEnd: React.PropTypes.instanceOf(Date),
+    dateFormat: React.PropTypes.string,
+    dateExclusions: React.PropTypes.array,
+    locales: React.PropTypes.array
   },
 
   getDefaultProps: function() {
+    var defaultDate = new Date();
+    defaultDate.setHours(12, 0, 0, 0);
+
     return {
-      includeTime: true
+      includeTime: true,
+      dateLabel: 'Date',
+      value: defaultDate,
+      locales: [ 'en-GB' ]
     };
+  },
+
+  defaultDate: function() {
+    var date = new Date();
+    date.setHours(12, 0, 0, 0);
+    return date;
+  },
+
+  timeChanged: function(newDateTime) {
+    if (this.props.onChange) {
+      this.props.onChange(newDateTime);
+    }
+  },
+
+  dateChanged: function(newMoment) {
+    if (this.props.onChange) {
+      var newDate = newMoment.toDate();
+      newDate.setHours(this.props.value.getHours(), this.props.value.getMinutes(), 0, 0);
+
+      this.props.onChange(newDate);
+    }
+  },
+
+  dateExclusions: function() {
+    if(!this.props.dateExclusions) {
+      return null;
+    }
+
+    return this.props.dateExclusions.map(function(date) {
+      return moment(date);
+    });
   },
 
   render: function() {
     var timePickerColumn = <span />;
 
+    // Should we mixin ReactIntl to ourselves so locales is pushed further down automatically?
+    // Do we need a ReactIntl compatible date picker for that to make sense, or to
+    // intercept it here to pass in their own (maybe Moment's) intl data?
     if (this.props.includeTime) {
       timePickerColumn = (
         <ReactBootstrap.Col xs={12} md={4}>
-          <TimePicker />
+          <TimePicker
+            className={this.props.timeClassName}
+            label={this.props.timeLabel}
+            name={this.props.timeName}
+            value={this.props.value}
+            onChange={this.timeChanged}
+            start={this.props.timeStart}
+            end={this.props.timeEnd}
+            step={this.props.timeStep}
+            locales={this.props.locales} />
         </ReactBootstrap.Col>
       );
     }
@@ -37,9 +102,17 @@ var DateTimeGroup = React.createClass({
         <ReactBootstrap.Row>
           <ReactBootstrap.Col xs={12} md={8}>
             <label className="control-label">
-              <span>Date</span>
+              <span>{this.props.dateLabel}</span>
             </label>
-            <DatePicker />
+            <DatePicker
+              selected={moment(this.props.value)}
+              onChange={this.dateChanged}
+              minDate={this.props.dateStart ? moment(this.props.dateStart) : null}
+              maxDate={this.props.dateEnd ? moment(this.props.dateEnd) : null}
+              excludeDates={this.dateExclusions()}
+              dateFormat={this.props.dateFormat}
+              dateFormatCalendar={this.props.dateFormat}
+              locale={this.props.locales[0]} />
           </ReactBootstrap.Col>
           {timePickerColumn}
         </ReactBootstrap.Row>
